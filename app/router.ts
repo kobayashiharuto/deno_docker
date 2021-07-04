17ec6621e513fbc938f196303e6b9536cdb869c5
kl <datesaver1234@gmail.com>
2021-07-04 20:50:17 +0900

    fix routing

--- app/router.ts
+++ app/router.ts
@@ -2,7 +2,7 @@ import { Router } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 import { getUsers } from "./controller.ts";
 
 const router = new Router();
-router.get("/books", getUsers);
+router.get("/users", getUsers);
 router.get("/", (context) => {
   context.response.body = "Hello API";
 });
